c57459722cec125c64bdb4ab57f7667c6ca8eaf3
Paul-Louis NECH <pln@algolia.com>
2017-03-02 12:01:19 +0100

    backend: done in PHP

---
+++ backend/backend.php
@@ -0,0 +1,30 @@
+<?php
+// STEP 1
+// Load dependency
+require __DIR__ . '/vendor/autoload.php';
+
+// Load json file
+$batch = json_decode(file_get_contents('../data/articles.json'), true);
+var_dump($batch);
+
+// Initialize API Client
+$client = new \AlgoliaSearch\Client('***REMOVED***', '***REMOVED***');
+
+// Initialize index
+$index = $client->initIndex('smashing');
+
+// Push data
+$index->addObjects($batch);
+
+// STEP 2
+// Set settings
+$index->setSettings(array(
+		"searchableAttributes" => array("title", "description", "tags", "author"),
+		"customRanking" => array("desc(commentCount)")));
+
+// Set settings and get settings
+$res = $index->setSettings(array("attributesForFaceting" => array("tags.name")));
+$index->waitTask($res['taskID']); // as operations are asynchronous, we explicitly wait for task completion
+$settings = $index->getSettings();
+var_dump($settings['attributesForFaceting']);
+?>
